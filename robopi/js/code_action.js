e01c536678656ec8419cfd76c969e7530f3b8c4c
tanvirs27 <rifat.csedu20@gmail.com>
2017-04-30 13:00:41 +0600

    motor test module added

--- robopi/js/code_action.js
+++ robopi/js/code_action.js
@@ -1,3 +1,18 @@
+Blockly.Python['action_test'] = function(block) {
+  
+	var speed_left = Blockly.Python.valueToCode(block, 'speed_left', Blockly.Python.ORDER_ATOMIC) || '255';
+  
+	var speed_right = Blockly.Python.valueToCode(block, 'speed_right', Blockly.Python.ORDER_ATOMIC) || '255';
+  
+	var rot_left = Blockly.Python.valueToCode(block, 'rot_left', Blockly.Python.ORDER_ATOMIC) || '255';
+
+	var rot_right = Blockly.Python.valueToCode(block, 'rot_right', Blockly.Python.ORDER_ATOMIC) || '255';
+  
+  
+  var code = 'testMotor('+speed_left+','+speed_right+','+rot_left+','+rot_right+')\n';
+  return code;
+};
+
 Blockly.Python['action_go'] = function(block) {
   var go_dropdown = block.getFieldValue('go_dropdown');
   var go_steps = Blockly.Python.valueToCode(block, 'go_steps', Blockly.Python.ORDER_ATOMIC) || '5';
