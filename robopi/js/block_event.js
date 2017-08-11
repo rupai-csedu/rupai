b38378380309ae325abdda2c58b2f56efce78f23
tanvirs27 <rifat.csedu20@gmail.com>
2017-08-12 02:43:28 +0600

    event blocks added

---
+++ robopi/js/block_event.js
@@ -0,0 +1,61 @@
+Blockly.Blocks['event_voice'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("When voice input is")
+        .appendField(new Blockly.FieldDropdown([["stop","voice_stop"], ["go left","voice_left"], ["go right","voice_right"]]), "voice_command");
+    this.appendStatementInput("voice_statements")
+        .setCheck(null);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(90);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
+
+
+Blockly.Blocks['event_light_left'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("When left is")
+        .appendField(new Blockly.FieldDropdown([["white","left_white"], ["black","left_black"]]), "light_left_dropdown");
+    this.appendStatementInput("light_left_statements")
+        .setCheck(null);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(90);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
+
+Blockly.Blocks['event_light_right'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("When right is")
+        .appendField(new Blockly.FieldDropdown([["white","right_white"], ["black","right_black"]]), "light_right_dropdown");
+    this.appendStatementInput("light_right_statements")
+        .setCheck(null);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(90);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
+
+
+Blockly.Blocks['event_light_centre'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField("When centre is")
+        .appendField(new Blockly.FieldDropdown([["white","centre_white"], ["black","centre_black"]]), "light_centre_dropdown");
+    this.appendStatementInput("light_centre_statements")
+        .setCheck(null);
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setColour(90);
+    this.setTooltip('');
+    this.setHelpUrl('');
+  }
+};
---
+++ robopi/js/code_event.js
@@ -0,0 +1,33 @@
+Blockly.Python['event_voice'] = function(block) {
+  var dropdown_voice_command = block.getFieldValue('voice_command');
+  var statements_voice_statements = Blockly.Python.statementToCode(block, 'voice_statements');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
+
+
+Blockly.Python['event_light_left'] = function(block) {
+  var dropdown_light_left_dropdown = block.getFieldValue('light_left_dropdown');
+  var statements_light_left_statements = Blockly.Python.statementToCode(block, 'light_left_statements');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
+
+
+Blockly.Python['event_light_right'] = function(block) {
+  var dropdown_light_right_dropdown = block.getFieldValue('light_right_dropdown');
+  var statements_light_right_statements = Blockly.Python.statementToCode(block, 'light_right_statements');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
+
+Blockly.Python['event_light_centre'] = function(block) {
+  var dropdown_light_centre_dropdown = block.getFieldValue('light_centre_dropdown');
+  var statements_light_centre_statements = Blockly.Python.statementToCode(block, 'light_centre_statements');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
